60c9f72c7a0b8192c7fd723b4d54c82d59f828a2
Gerixmus <gerixmuss@gmail.com>
2025-07-28 01:46:03 +0200

    feat: add file display logic

--- src/app/files/files.ts
+++ src/app/files/files.ts
@@ -10,17 +10,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './files.css'
 })
 export class Files {
-
-  fileName = '';
+  files: { name: string }[] = [];
 
   constructor(private http: HttpClient) {}
+
+   ngOnInit(): void {
+    this.fetchFiles()
+  }
 
   onFileSelected(event: any) {
     const file:File = event.target.files[0];
 
     if (file) {
-      this.fileName = file.name;
-
       const formData = new FormData();
 
       formData.append("file", file);
@@ -28,10 +29,21 @@ export class Files {
       const upload$ = this.http.post("http://localhost:3000/files", formData);
 
       upload$.subscribe({
-        next: (res) => console.log('Upload success:', res),
+        next: (res) => {
+          console.log('Upload success:', res);
+          this.fetchFiles();
+        },
         error: (err) => console.error('Upload failed:', err)
       });
     }
   }
 
+  fetchFiles(): void {
+    this.http.get<{ name: string }[]>("http://localhost:3000/files").subscribe({
+      next: (files) => {
+        this.files = files.filter(file => !file.name.includes('_summary.txt'));
+      },
+      error: (err) => console.error('Failed to fetch files:', err)
+    });
+  }
 }
