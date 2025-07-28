2a312aeb78370a602495cf2fce802f6881daf53a
Gerixmus <gerixmuss@gmail.com>
2025-07-28 22:22:06 +0200

    refactor: move documents overview to separate route

--- src/app/documents/documents.ts
+++ src/app/documents/documents.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-documents',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './documents.html',
   styleUrl: './documents.css'
 })
 export class Documents {
+  currentPage = 1;
+  itemsPerPage = 12;
+  files: { name: string }[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchFiles()
+  }
+
+  fetchFiles(): void {
+    this.http.get<{ name: string }[]>("http://localhost:3000/files").subscribe({
+      next: (files) => {
+        this.files = files.filter(file => !file.name.includes('_summary.txt'));
+      },
+      error: (err) => console.error('Failed to fetch files:', err)
+    });
+  }
+
+    get paginatedFiles() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.files.slice(start, start + this.itemsPerPage);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.files.length / this.itemsPerPage);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 }
--- src/app/upload/upload.ts
+++ src/app/upload/upload.ts
@@ -11,15 +11,7 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './upload.css'
 })
 export class Upload {
-  files: { name: string }[] = [];
-  currentPage = 1;
-  itemsPerPage = 12;
-
   constructor(private http: HttpClient) {}
-
-   ngOnInit(): void {
-    this.fetchFiles()
-  }
 
   onFileSelected(event: any) {
     const file:File = event.target.files[0];
@@ -32,34 +24,9 @@ export class Upload {
       const upload$ = this.http.post("http://localhost:3000/files", formData);
 
       upload$.subscribe({
-        next: (res) => {
-          console.log('Upload success:', res);
-          this.fetchFiles();
-        },
+        next: (res) => console.log('Upload success:', res),
         error: (err) => console.error('Upload failed:', err)
       });
     }
   }
-
-  fetchFiles(): void {
-    this.http.get<{ name: string }[]>("http://localhost:3000/files").subscribe({
-      next: (files) => {
-        this.files = files.filter(file => !file.name.includes('_summary.txt'));
-      },
-      error: (err) => console.error('Failed to fetch files:', err)
-    });
-  }
-
-  get paginatedFiles() {
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    return this.files.slice(start, start + this.itemsPerPage);
-  }
-
-  get totalPages() {
-    return Math.ceil(this.files.length / this.itemsPerPage);
-  }
-
-  changePage(page: number) {
-    this.currentPage = page;
-}
 }
